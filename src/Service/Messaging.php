8d6b9a512199a805c2a5e7346932a8c816fb8f1d
Maxime Authié <authie.maxime@orange.fr>
2023-07-24 19:23:48 +0200

    Mise au propre commentaires

--- src/Service/Messaging.php
+++ src/Service/Messaging.php
@@ -8,14 +8,14 @@
 
         public function sendEmail(string $login, string $password, string $emailAdress, string $subject, string $body, string $contactFirstName, string $contactLastName) {
     
-            //Load Composer's autoloader
+            //? Charger l'autoload
             require '../vendor/autoload.php';
-    
-            //Create an instance; passing `true` enables exceptions
+            
+            //? Créer une instance PHPMailer
             $mail = new PHPMailer(true);
     
             try {
-                //Server settings
+                //? Définir les paramètres serveur
                 $mail->SMTPDebug   = 0;                                      // Enable verbose debug output : permet de gérer le debogage -> mettre à 0 pour désactiver
                 $mail->isSMTP();                                             // Send using SMTP : pour dire qu'on utilise un server SMTP
                 $mail->Host        = 'ssl0.ovh.net';                         // Set the SMTP server to send through
@@ -25,25 +25,26 @@
                 $mail->SMTPSecure  = PHPMailer::ENCRYPTION_SMTPS;            // Enable implicit TLS encryption
                 $mail->Port        = 465;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
-                //Recipients
+                //? Définir les expéditeurs/destinataires
                 $mail->setFrom($login, mb_convert_encoding('Admin Cécilia Orsi Coaching', 'ISO-8859-1', 'UTF-8'));                                // Adresse de l'expéditeur + alias qui apparait dans la boite du destinataire
                 $mail->addAddress($emailAdress, $contactFirstName.' '.$contactLastName);        // Adresse mail du destinataire
                 
     
-                //Content
+                //? Définir le contenu du mail
                 $mail->isHTML(true);                                        //Set email format to HTML : voir https://www.alsacreations.com/tuto/lire/1533-Un-e-mail-en-HTML-responsive-multi-clients.html pour la mise en forme
                 $mail->Subject = $subject;                                  //Objet du mail
                 $mail->Body    = $body;               
                 
-                //Send the email
+                //? Envoyer l'email
                 $mail->send();
 
-                //Return to controller
+                //? Retour au contrôler
                 return 'The email has been sent';
-    
+                
+            //? En cas d'erreur inattendue, capter l'erreur rencontrée  
             } catch (Exception $error) {
     
-                //Catch error
+                //? Retourner les détails de l'erreur au contrôleur
                 return "The email could not be sent : {$mail->ErrorInfo}";
             }
         }
