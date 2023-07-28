e4420cc1a96d086dc8ee16b6c23772ec00ce3bd1
Maxime Authié <authie.maxime@orange.fr>
2023-07-28 18:49:34 +0200

    add, update & deleteCategory()

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Service\Utils;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CategoryController extends AbstractController
 {
     #[Route('/api/category/all', name: 'app_categories_api', methods: ['GET','OPTIONS'])]
-    public function getAllCategories(Request $request , CategoryRepository $categoryRepository): Response
-    {
+    public function getAllCategories(Request $request , CategoryRepository $categoryRepository): Response {
         try {
 
             //? Répondre uniquement aux requêtes OPTIONS avec les en-têtes appropriés
@@ -32,7 +35,7 @@ class CategoryController extends AbstractController
             //? Si aucune catégorie n'est présente dans la BDD
             if (!isset($categories)) {
                 return $this->json(
-                    ['erreur'=> 'Aucune catégorie présente dans la BDD.'],
+                    ['message'=> 'Aucune catégorie présente dans la BDD.'],
                     206, 
                     ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'GET'],
                     []
@@ -51,9 +54,228 @@ class CategoryController extends AbstractController
         } catch (\Exception $error) {
             //? Retourner un json poour détailler l'erreur inattendue
             return $this->json(
-                ['Error' => $error->getMessage()],
+                ['message' => $error->getMessage()],
                 400,
                 ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Methods' => 'POST, OPTIONS'], 
+                []
+            );
+        }
+    }
+
+    #[Route('/api/category/add', name: 'app_categories_add_api', methods: ['POST','OPTIONS'])]
+    public function addCategory(Request $request , CategoryRepository $categoryRepository, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface): Response {
+        try {
+    
+            //? Répondre uniquement aux requêtes OPTIONS avec les en-têtes appropriés
+            if ($request->isMethod('OPTIONS')) {
+                return new Response('', 204, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Methods' => 'POST, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization, access-control-allow-origin',
+                    'Access-Control-Max-Age' => '86400', 
+                ]);
+            }
+
+            //?Récupérer le contenu de la requête en provenance du front (tout ce qui se trouve dans le body de la requête)
+            $json = $request->getContent();
+
+            //?On vérifie si le json n'est pas vide
+            if (!$json) {
+                return $this->json(
+                    ['message' => 'Le json est vide ou n\'existe pas.'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'], 
+                    []
+                );
+            }
+
+            //? Sérializer le json (on le change de format json -> tableau)
+            $data = $serializerInterface->decode($json, 'json');
+
+            //? Nettoyer les donnée issues du json
+            $categoryName     = Utils::cleanInput($data['name']);
+            $categoryColor    = Utils::cleanInput($data['color']);
+            
+            //? Vérifier si la catégorie existe déjà
+            $category = $categoryRepository->findOneBy(['name_category'=>$categoryName]);
+            
+            if ($category) {
+                return $this->json(
+                    ['message' => 'La catégorie existe déjà dans la BDD.'],
+                    206,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'], 
+                    []
+                );
+            }
+
+            // ?Instancier un objet Category
+            $category = new Category();
+            $category->setNameCategory($categoryName);
+            $category->setColorCategory($categoryColor);
+
+            //? Persiter et flush des données pour les insérer en BDD
+            $entityManagerInterface->persist($category);
+            $entityManagerInterface->flush();
+
+            //? Renvoyer un json pour avertir que l'enregistrement à bien été effectué
+            return $this->json(
+                ['message'=> 'La catégorie à bien été ajoutée à la BDD'],
+                200, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'],
+                []);
+
+        //? En cas d'erreur inattendue, capter l'erreur rencontrée        
+        } catch (\Exception $error) {
+
+            //? Retourner un json poour détailler l'erreur inattendue
+            return $this->json(
+                ['erreumessager'=> 'Etat du json : '.$error->getMessage()],
+                400, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'],
+                []);
+        }
+    }
+
+    #[Route('/api/category/update', name: 'app_categories_update_api', methods: ['PATCH','OPTIONS'])]
+    public function updateCategory(Request $request , CategoryRepository $categoryRepository, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface): Response {
+        try {
+    
+            //? Répondre uniquement aux requêtes OPTIONS avec les en-têtes appropriés
+            if ($request->isMethod('OPTIONS')) {
+                return new Response('', 204, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Methods' => 'PATCH, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization, access-control-allow-origin',
+                    'Access-Control-Max-Age' => '86400', 
+                ]);
+            }
+
+            //?Récupérer le contenu de la requête en provenance du front (tout ce qui se trouve dans le body de la requête)
+            $json = $request->getContent();
+
+            //?On vérifie si le json n'est pas vide
+            if (!$json) {
+                return $this->json(
+                    ['message' => 'Le json est vide ou n\'existe pas.'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'], 
+                    []
+                );
+            }
+
+            //? Sérializer le json (on le change de format json -> tableau)
+            $data = $serializerInterface->decode($json, 'json');
+
+            //? Nettoyer les donnée issues du json
+            $id               = Utils::cleanInput($data['id']);
+            $categoryName     = Utils::cleanInput($data['name']);
+            $categoryColor    = Utils::cleanInput($data['color']);
+            
+            //? Vérifier si la catégorie existe déjà
+            $category = $categoryRepository->find($id);
+            
+            if (!$category) {
+                return $this->json(
+                    ['message' => 'La catégorie n\'existe pas dans la BDD.'],
+                    206,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'], 
+                    []
+                );
+            }
+
+            //? Instancier un objet Category
+            $category->setNameCategory($categoryName);
+            $category->setColorCategory($categoryColor);
+
+            //? Persiter et flush des données pour les insérer en BDD
+            $entityManagerInterface->persist($category);
+            $entityManagerInterface->flush();
+
+            //? Renvoyer un json pour avertir que l'enregistrement à bien été effectué
+            return $this->json(
+                ['message'=> 'La catégorie à bien été modifiée dans la BDD'],
+                200, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'],
+                []);
+
+        //? En cas d'erreur inattendue, capter l'erreur rencontrée        
+        } catch (\Exception $error) {
+
+            //? Retourner un json poour détailler l'erreur inattendue
+            return $this->json(
+                ['erreumessager'=> 'Etat du json : '.$error->getMessage()],
+                400, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'],
+                []);
+        }
+    }
+
+    #[Route('/api/category/delete', name: 'app_categories_delete_api', methods: ['DELETE','OPTIONS'])]
+    public function deleteCategory(Request $request , CategoryRepository $categoryRepository, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface): Response {
+        try {
+    
+            //? Répondre uniquement aux requêtes OPTIONS avec les en-têtes appropriés
+            if ($request->isMethod('OPTIONS')) {
+                return new Response('', 204, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Methods' => 'DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization, access-control-allow-origin',
+                    'Access-Control-Max-Age' => '86400', 
+                ]);
+            }
+
+            //?Récupérer le contenu de la requête en provenance du front (tout ce qui se trouve dans le body de la requête)
+            $json = $request->getContent();
+
+            //?On vérifie si le json n'est pas vide
+            if (!$json) {
+                return $this->json(
+                    ['message' => 'Le json est vide ou n\'existe pas.'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'DELETE'], 
+                    []
+                );
+            }
+
+            //? Sérializer le json (on le change de format json -> tableau)
+            $data = $serializerInterface->decode($json, 'json');
+
+            //? Nettoyer les donnée issues du json
+            $id = Utils::cleanInput($data['id']);
+            $name = Utils::cleanInput($data['name']);
+
+            //? Vérifier si la catégorie existe déjà
+            $category = $categoryRepository->find($id);
+            
+            if (!$category) {
+                return $this->json(
+                    ['message' => 'La catégorie n\'existe pas dans la BDD.'],
+                    206,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'DELETE'], 
+                    []
+                );
+            }
+
+            //? Remove et flush des données pour les insérer en BDD
+            $entityManagerInterface->remove($category);
+            $entityManagerInterface->flush();
+
+            //? Renvoyer un json pour avertir que l'enregistrement à bien été effectué
+            return $this->json(
+                ['message'=> 'La catégorie '.$name.' à bien été supprimée de la BDD'],
+                200, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'DELETE'],
+                []
+            );
+
+        //? En cas d'erreur inattendue, capter l'erreur rencontrée        
+        } catch (\Exception $error) {
+
+            //? Retourner un json poour détailler l'erreur inattendue
+            return $this->json(
+                ['erreumessager'=> 'Etat du json : '.$error->getMessage()],
+                400, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'DELETE'],
                 []
             );
         }
