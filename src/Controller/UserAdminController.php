06d405dccfd682ee6105d468e155eaa32ea85e1f
Maxime Authié <authie.maxime@orange.fr>
2023-08-04 19:19:31 +0200

    ajout getAllUsers() + updateUser()

--- src/Controller/UserAdminController.php
+++ src/Controller/UserAdminController.php
@@ -2,17 +2,157 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\Utils;
+use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserAdminController extends AbstractController
 {
-    #[Route('/user/admin', name: 'app_user_admin')]
-    public function index(): Response
-    {
-        return $this->render('user_admin/index.html.twig', [
-            'controller_name' => 'UserAdminController',
-        ]);
+    #[Route('/api/user/all', name: 'app_users_all_api', methods: ['GET','OPTIONS'])]
+    public function getAllUsers(Request $request , UserRepository $userRepository): Response {
+        try {
+
+            //? Répondre uniquement aux requêtes OPTIONS avec les en-têtes appropriés
+            if ($request->isMethod('OPTIONS')) {
+                
+                return new Response('', 204, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization, access-control-allow-origin',
+                    'Access-Control-Max-Age' => '86400', 
+                ]);
+            }
+
+            //? Rechercher les catégories dans la base de données
+            $users = $userRepository->findAll();
+
+            //? Si aucune catégorie n'est présente dans la BDD
+            if (!isset($users)) {
+                return $this->json(
+                    ['message'=> 'Aucun utilisateur présent dans la BDD.'],
+                    206, 
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'GET'],
+                    []
+                );
+            }
+
+            //? Si des users sont présents dans la BDD
+            return $this->json(
+                $users, 
+                200, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'GET'], 
+                ['groups' => 'user:getAll']
+            ); 
+
+        //? En cas d'erreur inattendue, capter l'erreur rencontrée
+        } catch (\Exception $error) {
+            //? Retourner un json poour détailler l'erreur inattendue
+            return $this->json(
+                ['message' => $error->getMessage()],
+                400,
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Methods' => 'POST, OPTIONS'], 
+                []
+            );
+        }
+    }
+
+    #[Route('/api/user/update', name: 'app_user_update_api', methods: ['PATCH','OPTIONS'])]
+    public function updateUser(Request $request , UserRepository $userRepository,SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, UserPasswordHasherInterface $userPasswordHasherInterface): Response {
+        try {
+    
+            //? Répondre uniquement aux requêtes OPTIONS avec les en-têtes appropriés
+            if ($request->isMethod('OPTIONS')) {
+                return new Response('', 204, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Methods' => 'PATCH, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization, access-control-allow-origin',
+                    'Access-Control-Max-Age' => '86400', 
+                ]);
+            }
+
+            //?Récupérer le contenu de la requête en provenance du front (tout ce qui se trouve dans le body de la requête)
+            $json = $request->getContent();
+
+            //?On vérifie si le json n'est pas vide
+            if (!$json) {
+                return $this->json(
+                    ['message' => 'Le json est vide ou n\'existe pas.'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'], 
+                    []
+                );
+            }
+
+            //? Sérializer le json (on le change de format json -> tableau)
+            $data = $serializerInterface->decode($json, 'json');
+
+            //? Nettoyer les données issues du json et les stocker dans des variables
+            $id                         = Utils::cleanInput($data['id']);
+            $firstName                  = Utils::cleanInput($data['first_name_user']);
+            $lastName                   = Utils::cleanInput($data['last_name_user']);
+            $email                      = Utils::cleanInput($data['email']);
+            $roles                      = $data['roles'];
+            $password                   = Utils::cleanInputArticleContent($data['password_user']);
+
+            //? Vérifier si l'article existe
+            $user = $userRepository->find($id);
+            
+            if (!$user) {
+                return $this->json(
+                    ['message' => 'La l\'utilisateur n\'existe pas dans la BDD.'],
+                    206,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'], 
+                    []
+                );
+            }
+
+            //? Vérifier si le format de l'adresse mail est valide
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            
+                return $this->json(
+                    ['Error' => 'The email adress '.$data['email'].' is not a valid email adress.'],
+                    422,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );
+            }
+        
+            //? Instancier un objet User et setter ses propriétés
+            $user->setFirstNameUser($firstName);
+            $user->setLastNameUser($lastName);
+            $user->setRoles($roles);
+
+            //? Si un nouveau mot de passe existe dans le json, setter le password
+            if(isset($password)) {
+                $user->setPassword($userPasswordHasherInterface->hashPassword($user, $password));
+            }
+
+            //? Persiter et flush des données pour les insérer en BDD
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+
+            //? Renvoyer un json pour avertir que l'enregistrement à bien été effectué
+            return $this->json(
+                ['message'=> 'L\'utilisateur à bien été mis à jour dans la BDD'],
+                200, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'],
+                []);
+
+        //? En cas d'erreur inattendue, capter l'erreur rencontrée        
+        } catch (\Exception $error) {
+
+            //? Retourner un json poour détailler l'erreur inattendue
+            return $this->json(
+                ['erreumessager'=> 'Etat du json : '.$error->getMessage()],
+                400, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'],
+                []);
+        }
     }
 }
