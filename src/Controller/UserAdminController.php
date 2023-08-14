5412d296b51b6d90ef05c48739edfa2aa775bff0
Maxime Authié <authie.maxime@orange.fr>
2023-08-14 18:57:38 +0200

    ajout addUser() + nettoyage role de updateUser()

--- src/Controller/UserAdminController.php
+++ src/Controller/UserAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -94,13 +95,20 @@ class UserAdminController extends AbstractController
 
             //? Nettoyer les données issues du json et les stocker dans des variables
             $id                         = Utils::cleanInput($data['id']);
-            $firstName                  = Utils::cleanInput($data['first_name_user']);
-            $lastName                   = Utils::cleanInput($data['last_name_user']);
-            $email                      = Utils::cleanInput($data['email']);
-            $roles                      = $data['roles'];
-            $password                   = Utils::cleanInputArticleContent($data['password_user']);
+            $firstName                  = Utils::cleanInput($data['firstName']);
+            $lastName                   = Utils::cleanInput($data['lastName']);
+            $email                      = Utils::cleanInput($data['email']);     
+            $password                   = Utils::cleanInput($data['password']);
 
-            //? Vérifier si l'article existe
+            //? Nettoyer les rôles issus du json
+            $roles                      = [];
+
+            foreach ($data['roles'] as $item) {
+                $roles[] = Utils::cleanInput($item);   
+            }
+
+
+            //? Vérifier si l'utilisateur existe
             $user = $userRepository->find($id);
             
             if (!$user) {
@@ -116,9 +124,9 @@ class UserAdminController extends AbstractController
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             
                 return $this->json(
-                    ['Error' => 'The email adress '.$data['email'].' is not a valid email adress.'],
+                    ['message' => 'Le format de l\'adresse email n\'est pas valide.'],
                     422,
-                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'GET'], 
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'], 
                     []
                 );
             }
@@ -126,6 +134,7 @@ class UserAdminController extends AbstractController
             //? Instancier un objet User et setter ses propriétés
             $user->setFirstNameUser($firstName);
             $user->setLastNameUser($lastName);
+            $user->setEmail($email);
             $user->setRoles($roles);
 
             //? Si un nouveau mot de passe existe dans le json, setter le password
@@ -149,9 +158,107 @@ class UserAdminController extends AbstractController
 
             //? Retourner un json poour détailler l'erreur inattendue
             return $this->json(
-                ['erreumessager'=> 'Etat du json : '.$error->getMessage()],
+                ['message'=>'Etat du json : '.$error->getMessage()],
                 400, 
                 ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'PATCH'],
+                []);
+        }
+    }
+
+    #[Route('/api/user/add', name: 'app_user_add_api', methods: ['POST','OPTIONS'])]
+    public function addUser(Request $request , UserRepository $userRepository,SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, UserPasswordHasherInterface $userPasswordHasherInterface): Response {
+        try {
+    
+            //? Répondre uniquement aux requêtes OPTIONS avec les en-têtes appropriés
+            if ($request->isMethod('OPTIONS')) {
+                return new Response('', 204, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Methods' => 'POST, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization, access-control-allow-origin',
+                    'Access-Control-Max-Age' => '86400', 
+                ]);
+            }
+
+            //?Récupérer le contenu de la requête en provenance du front (tout ce qui se trouve dans le body de la requête)
+            $json = $request->getContent();
+
+            //?On vérifie si le json n'est pas vide
+            if (!$json) {
+                return $this->json(
+                    ['message' => 'Le json est vide ou n\'existe pas.'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'], 
+                    []
+                );
+            }
+
+            //? Sérializer le json (on le change de format json -> tableau)
+            $data = $serializerInterface->decode($json, 'json');
+
+            //? Nettoyer les données issues du json et les stocker dans des variables
+            $firstName                  = Utils::cleanInput($data['firstName']);
+            $lastName                   = Utils::cleanInput($data['lastName']);
+            $email                      = Utils::cleanInput($data['email']);     
+            $password                   = Utils::cleanInput($data['password']);
+
+            //? Nettoyer les rôles issus du json
+            $roles                      = [];
+
+            foreach ($data['roles'] as $item) {
+                $roles[] = Utils::cleanInput($item);   
+            }
+
+
+            //? Vérifier si l'utilisateur existe
+            $user = $userRepository->findOneBy(['email'=>$email]);
+            
+            if ($user) {
+                return $this->json(
+                    ['message' => 'La l\'utilisateur '.$email.' existe déjà dans la BDD.'],
+                    206,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'], 
+                    []
+                );
+            }
+
+            //? Vérifier si le format de l'adresse mail est valide
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            
+                return $this->json(
+                    ['message' => 'Le format de l\'adresse email n\'est pas valide.'],
+                    422,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'], 
+                    []
+                );
+            }
+        
+            //? Instancier un objet User et setter ses propriétés
+            $newUser = new User();
+            $newUser->setFirstNameUser($firstName);
+            $newUser->setLastNameUser($lastName);
+            $newUser->setEmail($email);
+            $newUser->setPassword($userPasswordHasherInterface->hashPassword($newUser, $password));
+            $newUser->setRoles($roles);
+
+            //? Persiter et flush des données pour les insérer en BDD
+            $entityManagerInterface->persist($newUser);
+            $entityManagerInterface->flush();
+
+            //? Renvoyer un json pour avertir que l'enregistrement à bien été effectué
+            return $this->json(
+                ['message'=> 'L\'utilisateur à bien été ajouté à la BDD'],
+                200, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'],
+                []);
+
+        //? En cas d'erreur inattendue, capter l'erreur rencontrée        
+        } catch (\Exception $error) {
+
+            //? Retourner un json poour détailler l'erreur inattendue
+            return $this->json(
+                ['message'=>'Etat du json : '.$error->getMessage()],
+                400, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'*', 'Access-Control-Allow-Method' => 'POST'],
                 []);
         }
     }
