9b0503f92cbcc81282a35a686ab9c24af0f39be9
Maxime Authié <authie.maxime@orange.fr>
2023-08-18 21:34:58 +0200

    changement frns mail

--- src/Service/Messaging.php
+++ src/Service/Messaging.php
@@ -6,7 +6,7 @@
 
     class Messaging {
 
-        public function sendEmail(string $login, string $password, string $emailAdress, string $subject, string $body, string $contactFirstName, string $contactLastName) {
+        public function sendEmail(string $login, string $password, string $emailAdress, string $subject, string $body, string|null $contactFirstName, string|null $contactLastName) {
     
             //? Charger l'autoload
             require '../vendor/autoload.php';
@@ -18,12 +18,12 @@
                 //? Définir les paramètres serveur
                 $mail->SMTPDebug   = 0;                                      // Enable verbose debug output : permet de gérer le debogage -> mettre à 0 pour désactiver
                 $mail->isSMTP();                                             // Send using SMTP : pour dire qu'on utilise un server SMTP
-                $mail->Host        = 'ssl0.ovh.net';                         // Set the SMTP server to send through
+                $mail->Host        = 'smtp.orange.fr';                         // Set the SMTP server to send through
                 $mail->SMTPAuth    = true;                                   // Enable SMTP authentication
                 $mail->Username    = $login;                                 // SMTP username
                 $mail->Password    = $password;                              // SMTP password
-                $mail->SMTPSecure  = PHPMailer::ENCRYPTION_SMTPS;            // Enable implicit TLS encryption
-                $mail->Port        = 465;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                $mail->SMTPSecure  = PHPMailer::ENCRYPTION_STARTTLS;            // Enable implicit TLS encryption //! PHPMailer::ENCRYPTION_SMTPS ave ovh et port 465
+                $mail->Port        = 587;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
                 //? Définir les expéditeurs/destinataires
                 $mail->setFrom($login, mb_convert_encoding('Admin Cécilia Orsi Coaching', 'ISO-8859-1', 'UTF-8'));                                // Adresse de l'expéditeur + alias qui apparait dans la boite du destinataire
